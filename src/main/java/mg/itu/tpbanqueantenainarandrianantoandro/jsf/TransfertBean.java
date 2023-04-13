bb42ce163aba88fec96e81f2d8f911a0049d77b0
antenaina04 <antenainarandrianantoandro@gmail.com>
2023-04-14 00:04:05 +0300

    Transfert d'argent d'un compte à l'autre avec gestions d'erreurs ➕

--- src/main/java/mg/itu/tpbanqueantenainarandrianantoandro/jsf/TransfertBean.java
+++ src/main/java/mg/itu/tpbanqueantenainarandrianantoandro/jsf/TransfertBean.java
@@ -7,6 +7,9 @@ package mg.itu.tpbanqueantenainarandrianantoandro.jsf;
 import jakarta.ejb.EJB;
 import jakarta.inject.Named;
 import jakarta.enterprise.context.RequestScoped;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
+import jakarta.faces.validator.ValidatorException;
 import mg.itu.tpbanqueantenainarandrianantoandro.ejb.GestionnaireCompte;
 import mg.itu.tpbanqueantenainarandrianantoandro.entities.CompteBancaire;
 
@@ -53,8 +56,50 @@ public class TransfertBean {
     }
 
     public String update() {
+        boolean erreur = false;
+        if (source.getId() == null || destination.getId() == null || source.getId() == 0 || destination.getId() == 0) {
+            FacesMessage message
+                    = new FacesMessage("Le destinataire ou l'expediteur n'existe pas");
+            FacesContext.getCurrentInstance().addMessage("source-destination-null", message);
+            erreur = true;
+        }
+        if (source.getSolde() < montant) {
+            FacesMessage message
+                    = new FacesMessage("Solde insuffisant");
+            FacesContext.getCurrentInstance().addMessage("source-insuffisant", message);
+            erreur = true;
+        }
+        if (erreur) {
+            return null;
+        }
+//
+//        boolean erreur = false;
+//        CompteBancaire source = gestionnaireCompte.getCompte(idSource);
+//        if (source == null) {
+//            // Message d'erreur associé au composant source ; form:source est l'id client
+//            // si l'id du formulaire est "form" et l'id du champ de saisie de l'id de la source est "source"
+//            // dans la page JSF qui lance le transfert.
+//            Util.messageErreur("Aucun compte avec cet id !", "Aucun compte avec cet id !", "form:source");
+//            erreur = true;
+//        } else {
+//            if (source.getSolde() < montant) {
+//
+//                erreur = true;
+//            }
+//        }
+//
+//        if (erreur) { // en cas d'erreur, rester sur la même page
+//            return null;
+//        }
+//        gestionnaireCompte.tranferer(source, destination, montant);
+//
+//        Util.addFlashInfoMessage("Transfert correctement effectué");
+
+// Message de succès ; addFlash à cause de la redirection.
         transfert.transferer(source, destination, montant);
+        Util.addFlashInfoMessage("Transfert correctement effectué");
 
         return "listeComptes.xhtml?faces-redirect=true";
+
     }
 }
